eac6f03cd93f9010f7c6cfef97b0d77533b9e5ca
Nilavan Rajamani <nilavanrajamani@gmail.com>
2024-01-06 14:55:32 +0530

    removed auth

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer l5UqN-4mf3H8kyKJwgNADfodxwLBIIYnY6XcNHNGS582VkvGwEH3AnnRyK68IR74zw7j2kYMF1GFXhmwWsfTp9XKZoucxm2xXXR5LjR6-TdQrJoWZGN9kpSTaUWJZXYx'
+        Authorization: 'Bearer #####'
     }
 });
